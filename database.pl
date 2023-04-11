bdd3a1bb3c0ee7bd4dcacc46666649ff97f5ea77
frehlid <frehlid@student.ubc.ca>
2023-04-10 20:05:24 -0700

    Changes from monday session

--- database.pl
+++ database.pl
@@ -1,13 +1,45 @@
 :- use_module(library(http/http_open)).
 :- use_module(library(http/json)).
+:- use_module(library(apply)).
+:- use_module(library(http/http_open)).
+:- use_module(library(http/json)).
+:- use_module(library(http/http_client)).
 
-request_weather(JSON) :-
-    setup_call_cleanup(
-       http_open('http://api.weatherapi.com/v1/current.json?key=d7343302f550499f88f11158230704&q=Vancouver', In, []),
-       json_read_dict(In, JSON),
-       close(In)
-   ).
+% Wikidata endpoint URL
+wdqs_url('https://query.wikidata.org/sparql').
 
+% SPARQL query to check if a city exists in Wikidata
+query(City, Sparql) :-
+    atomic_list_concat(['SELECT ?city WHERE { ?city wdt:P31/wdt:P279* wd:Q515. ?city rdfs:label "', City, '"@en. }'], Sparql).
+
+replace_spaces_with_dash(String, Result) :-
+    atomic_list_concat(Words, ' ', String),
+    atomic_list_concat(Words, '-', Result).
+
+%request_weather(City, JSON) :-
+%    replace_spaces_with_dash(City, CityDash),
+%    atom_concat('http://api.weatherapi.com/v1/current.json?key=d7343302f550499f88f11158230704&q=', CityDash, URL),
+%    setup_call_cleanup(
+%       http_open(URL, In, []),
+%       json_read_dict(In, JSON),
+%       close(In)
+%   ).
+request_weather(City, JSON) :-
+    replace_spaces_with_dash(City, CityDash),
+    atom_concat('http://api.weatherapi.com/v1/forecast.json?key=d7343302f550499f88f11158230704&q=', CityDash, URL),
+    catch(
+        setup_call_cleanup(
+            http_open(URL, In, []),
+            json_read_dict(In, JSON),
+            close(In)
+        ),
+     error(_,http_error(_,_,_)),
+     JSON = false).
+
+
+
+
+:- dynamic(location/1).
 :- dynamic(temperature/1).
 :-dynamic(wind_speed/1).
 :- dynamic(wind_gust/1).
@@ -20,10 +52,67 @@ request_weather(JSON) :-
 :-dynamic(visibility/1).
 :-dynamic(uv/1).
 
+:-dynamic(maxtemp/1).
+:-dynamic(mintemp/1).
+:-dynamic(avgtemp/1).
+:-dynamic(maxwind/1).
+:-dynamic(will_it_rain/1).
+:-dynamic(chance_of_rain/1).
+:-dynamic(will_it_snow/1).
+:-dynamic(chance_of_snow/1).
+
+
+units(pressure(_), "mb").
+units(temperature(_), "C").
+units(wind_speed(_), "kph").
+units(precipitation(_), "mm").
+units(humidity(_), "%").
+units(cloud(_), "%").
+units(feelslike(_), "C").
+units(visibility(_), "km").
+units(uv(_), "UV Index").
+units(maxtemp(_), "C").
+units(avgtemp(_), "C").
+units(mintemp(_), "C").
+units(will_it_rail(_), "0/1").
+units(will_it_snow(_), "0/1").
+units(chance_of_rain(_), "%").
+units(chance_of_snow(_), "%").
+units(sunny(_), "%").
+
+
+explanation(Noun, Constraint, Ind, Exp) :-
+    atom_concat("The ", Noun, First),
+    atom_concat(First, " is ", Second),
+    atom_concat(Second, Ind, PrintAns0),
+    atom_concat(PrintAns0, " ", PrintAns1),
+    units(Constraint, Unit),
+    atom_concat(PrintAns1, Unit, Exp).
+
+
+
+get_explanation("umbrella", _, Exp) :-
+    chance_of_rain(Chance),
+    explanation("chance of rain", chance_of_rain(_), Chance, Exp).
+
+get_explanation("coat", _, Exp) :-
+    temperature(Temp),
+    explanation("temperature", temperature(_), Temp, Exp).
+
+get_explanation("sunglasses", _, Exp) :-
+    cloud(Clouds),
+    explanation("cloud cover", cloud(_), Clouds, Exp).
+
+get_explanation("raincoat", _, Exp) :-
+    chance_of_rain(Chance),
+    explanation("chance of rain", chance_of_rain(_), Chance, Exp).
+
+
+location("").
 temperature(0).
 wind_speed(0).
 wind_gust(0).
-wind_direction("E").
+
 pressure(0). % mb
 precipitation(0). % mm
 humidity(0).
@@ -32,7 +121,16 @@ feelslike(0).
 visibility(0). % km
 uv(0).
 
-get_current(JSON, CW) :- CW = JSON.get(current). 
+maxtemp(0).
+mintemp(0).
+avgtemp(0).
+maxwind(0).
+will_it_rain(0).
+chance_of_rain(0).
+will_it_snow(0).
+chance_of_snow(0).
+
+get_current(JSON, CW) :- CW = JSON.get(current).
 
 update_database(Name, Value) :-
    Fact1 =.. [Name, _],
@@ -53,9 +151,41 @@ update_weather(CW) :-
    update_database(visibility, CW.get(vis_km)),
    update_database(uv, CW.get(uv)).
 
-add_facts_to_database(_):-
-   request_weather(JSON),
-   get_current(JSON, CW),
-   update_weather(CW).
+get_day(JSON, Day) :- Forecast = JSON.get(forecast), [FD | _] = Forecast.get(forecastday), Day = FD.get(day).
 
+update_forecast(Day) :-
+    update_database(maxtemp, Day.get(maxtemp_c)),
+    update_database(mintemp, Day.get(mintemp_c)),
+    update_database(avgtemp, Day.get(avgtemp_c)),
+    update_database(maxwind, Day.get(maxwind_kph)),
+    update_database(will_it_rain, Day.get(daily_will_it_rain)),
+    update_database(will_it_snow, Day.get(daily_will_it_snow)),
+    update_database(chance_of_rain, Day.get(daily_chance_of_rain)),
+    update_database(chance_of_snow, Day.get(daily_chance_of_snow)).
+
+
+%add_facts_to_database(City):-
+%   request_weather(City, JSON),
+%   get_current(JSON, CW),
+%   update_weather(CW).
+
+equal_ignore_case(S1, S2) :-
+    downcase_atom(S1, S1Lower),
+    downcase_atom(S2, S2Lower),
+    S1Lower = S2Lower.
+
+
+add_facts_to_database(City) :-
+    catch(request_weather(City, JSON), _, fail),
+    Location = JSON.get(location),
+    CityR = Location.get(name),
+    (equal_ignore_case(CityR, City) ->
+        update_database(location, City),
+        get_current(JSON, CW),
+        update_weather(CW),
+        get_day(JSON, Day),
+        update_forecast(Day)
+    ;
+        fail
+    ).
 
--- weather_database.pl
+++ weather_database.pl
@@ -3,7 +3,6 @@
 	Currently, the information is just hard-coded as a database.
 */
 
-
 % database is loaded from
 :- [database].
 
@@ -24,6 +23,12 @@ noun_phrase(L0,L4,Ind,C0,C4) :-
     det(L0,L1,Ind,C3,C4),
     adjectives(L1,L2,Ind,C2,C3),
     noun(L2,L3,Ind,C1,C2),
+    omp(L3,L4,Ind,C0,C1).
+
+noun_phrase(L0,L4,Ind,C0,C4) :-
+    det(L0,L1,Ind,C3,C4),
+    adjectives(L1,L2,Ind,C2,C3),
+    pronoun(L2,L3,Ind,C1,C2),
     omp(L3,L4,Ind,C0,C1).
 
 % ---------------- Question Structure ----------------
@@ -56,48 +61,79 @@ adj(["cold" | L],L,Ind, [cold(Ind) | C],C).
 adj(["warm" | L],L,Ind, [warm(Ind) | C],C).
 adj(["windy" | L],L,Ind, [windy(Ind) | C],C).
 adj(["humid" | L],L,Ind, [humid(Ind) | C],C).
+adj(["cloudy" | L],L,Ind, [cloudy(Ind) | C],C).
+adj(["rainy" | L],L,Ind, [rainy(Ind) | C],C).
+adj(["sunny" | L],L,Ind, [sunny(Ind) | C],C).
 
+adj_to_noun("cold", T, "temperature") :- temperature(T).
+adj_to_noun("warm", T, "temperature") :- temperature(T).
+adj_to_noun("windy", S,  "wind speed") :- wind_speed(S).
+adj_to_noun("humid", H, "humidity") :- humidity(H).
+adj_to_noun("cloudy", C, "cloud cover") :- cloud(C).
+adj_to_noun("snowy", S, "chance of snow") :- chance_of_snow(S).
+adj_to_noun("rainy", R, "chance of rain") :- chance_of_rain(R).
+adj_to_noun("sunny", C, "cloud cover") :- cloud(C).
 
 noun(["temperature" | L],L,Ind, [temperature(Ind) | C],C).
 noun(["wind", "speed" | L],L,Ind, [wind_speed(Ind) | C],C).
-noun(["humidity" | L],L,Ind, [humidity(_,Ind) | C],C).
-noun(["pressure" | L],L,Ind, [pressure(_,Ind) | C],C).
-noun(["wind", "chill" | L],L,Ind, [feels_like(_,Ind) | C],C).
-
-noun(["Vancouver" |L], L, _, C, C).
+noun(["wind", "gust" | L],L,Ind, [wind_gust(Ind) | C],C).
+noun(["wind", "direction" | L],L,Ind, [wind_direction(Ind) | C],C).
+noun(["humidity" | L],L,Ind, [humidity(Ind) | C],C).
+noun(["pressure" | L],L,Ind, [pressure(Ind) | C],C).
+noun(["wind", "chill" | L],L,Ind, [feelslike(Ind) | C],C).
+noun(["visibility" | L],L,Ind, [visibility(Ind) | C],C).
+noun(["uv" | L],L,Ind, [uv(Ind) | C],C).
+noun(["ultraviolet" | L],L,Ind, [uv(Ind) | C],C).
+noun(["cloud" | L],L,Ind, [cloud(Ind) | C],C).
+noun(["cloud cover" | L], L, Ind, [cloud(Ind) | C],C).
+noun(["precipitation" | L],L,Ind, [precipitation(Ind) | C],C).
+noun(["chance of rain" | L],L,Ind, [chance_of_rain(Ind) | C],C).
+noun(["chance of snow" | L],L,Ind, [chance_of_rain(Ind) | C],C).
 
 % personal items
-noun(["umbrella" | L], L, umbrella, C, C).
-noun(["coat" | L], L, coat, C, C).
+noun(["umbrella" | L], L, Ind, [umbrella(Ind) | C], C).
+noun(["coat" | L], L, Ind, [coat(Ind) | C], C).
+noun(["raincoat" | L], L, Ind, [raincoat(Ind) | C], C).
+noun(["sunglasses" | L], L, Ind, [sunny(Ind) | C], C).
 
-%noun(["umbrella" | L],L,Ind, [umbrella(Ind) | C],C).
+verb_phrase(["is", "it" | L], L, _, C, C).
+
+pronoun(["it" |L], L, _, C, C).
 
 reln(["in" | L],L,Ind,Ind,C,C).
 reln(["the", "temperature", "in" | L],L,Sub,_, [temperature(Sub)|C],C).
 reln(["the", "wind", "speed", "in" | L],L,Sub,_, [wind_speed(Sub)|C],C).
-reln(["the", "humidity", "in" | L],L,Sub,Obj, [humidity(Obj,Sub)|C],C).
-reln(["the", "humidity", "in" | L],L,Sub,Obj, [humidity(Obj,Sub)|C],C).
-reln(["the", "pressure", "in" | L],L,Sub,Obj, [pressure(Obj,Sub)|C],C).
-reln(["the", "wind", "chill", "in" | L],L,Sub,Obj, [feels_like(Obj,Sub)|C],C).
+reln(["the", "humidity", "in" | L],L,Sub,_, [humidity(Sub)|C],C).
+reln(["the", "humidity", "in" | L],L,Sub,_, [humidity(Sub)|C],C).
+reln(["the", "pressure", "in" | L],L,Sub,_, [pressure(Sub)|C],C).
+reln(["the", "wind", "chill", "in" | L],L,Sub,_, [feels_like(Sub)|C],C).
 
 % ---------------- Asking questions ----------------
 % a question can be an auxiliary verb followed by subject and then an adjective
 % - Is Vancouver cold?
 % - Is Vancouver warm?
 % - Is Vancouver windy?
-question(["Is" | L0],L3,Ind,C0,C3) :-
+question(["is" | L0],L3,Ind,C0,C3) :-
     aphrase(L0,L1, Ind, C0,C1),
     adj(L1,L2,Ind,C1,C2),
     omp(L2, L3, Ind, C2, C3).
 
-%changed this to call aphrase, in case subject is not city
-question(["What", "is" | L0],L1,Ind,C0,C1) :-
+%question(["is" | L0], L2, Ind, C0, C2) :-
+%    aphrase(L0,L1, Ind, C0,C1),
+%    verb(L1,L2,Ind,C1,C2).
+
+question(["what", "is" | L0],L1,Ind,C0,C1) :-
     aphrase(L0,L1,Ind,C0,C1).
-
-%question(["Do", "I", "need" | L0],L1,Ind,C0,C1).
-
-question(["Should", "I", "wear" | L0],L1,Ind,C0,C1) :-
+question(["do", "i", "need" | L0],L1,Ind,C0,C1) :-
     noun_phrase(L0, L1, Ind, C0, C1).
+question(["should", "i", "bring" | L0],L1,Ind,C0,C1) :-
+    noun_phrase(L0, L1, Ind, C0, C1).
+question(["should", "i", "wear" | L0],L1,Ind,C0,C1) :-
+    noun_phrase(L0, L1, Ind, C0, C1).
+
+question(["how" | L0],L2,Ind,C0,C2) :-
+    adj(L0,L1,Ind,C0,C1),
+    verb_phrase(L1, L2, Ind, C1, C2).
 
 % ask(Q,A) gives answer A to question Q
 ask(Q,A) :-
@@ -110,12 +146,77 @@ get_constraints_from_question(Q,A,C) :-
     member(End,[[],["?"],["."]]). % check that the question ends with a period
 
 
+:- discontiguous prove_all/4.
 % prove_all(L) is true if all elements of L can be proved from the knowledge base
-prove_all(_,[],_, _).
-prove_all(["Is"| _],[H|T],_, _) :-
-    call(H),      % built-in Prolog predicate calls an atom
-    prove_all(["Is"| _], T, _, _).
 
+prove_all(["is"| _],[Last],_, _) :-
+    \+ call(Last),
+    adj([Adj | _],_,_, [Last | _],_),
+    atom_concat("No, it is not ", Adj, PrintAns1),
+    atom_concat(PrintAns1, "\n", PrintAns),
+    write(PrintAns),
+    flush_output(current_output),
+    adj_to_noun(Adj, Val, Noun),
+    noun([Noun | _], _, _, [Constraint | _], _),
+    explanation(Noun, Constraint, Val, PrintExp),
+    write(PrintExp), flush_output(current_output),
+    fail.
+
+prove_all(["is"| _],[Last],_, _) :-
+    call(Last),
+    adj([Adj | _],_,_, [Last | _],_),
+    atom_concat("Yes, it is ", Adj, PrintAns1),
+    atom_concat(PrintAns1, "\n", PrintAns),
+    write(PrintAns), flush_output(current_output),
+    adj_to_noun(Adj, Val, Noun),
+    noun([Noun | _], _, _, [Constraint | _], _),
+    explanation(Noun, Constraint, Val, PrintExp),
+    write(PrintExp), flush_output(current_output).
+
+accessory_questions(Last,  DoOrShould, BringOrWear) :-
+    call(Last),
+    noun([Noun | _],_,_, [Last | _],_),
+    format(atom(PrintAns), "Yes, you ~s ~s a(n) ~s. ", [DoOrShould, BringOrWear, Noun]),
+    write(PrintAns),
+    flush_output(current_output),
+    get_explanation(Noun, _, Exp),
+    write(Exp), flush_output(current_output).
+
+accessory_questions(Last, DoOrShould, BringOrWear) :-
+    \+ call(Last),
+    noun([Noun | _],_,_, [Last | _],_),
+    format(atom(PrintAns), "No, you ~s not ~s a(n) ~s. ", [DoOrShould, BringOrWear, Noun]),
+    write(PrintAns),
+    flush_output(current_output),
+    get_explanation(Noun, _, Exp),
+    write(Exp), flush_output(current_output),
+    fail.
+
+prove_all(["should", "i", "bring" | _], [Last],_, _) :-
+    accessory_questions(Last, "should", "bring").
+
+prove_all(["should", "i", "wear" | _], [Last],_, _) :-
+    accessory_questions(Last, "should", "wear").
+
+prove_all(["do", "i", "need" | _], [Last],_, _) :-
+    accessory_questions(Last, "do", "need").
+
+%prove_all(["Is"| _],[H|T],_, _) :-
+%    call(H),      % built-in Prolog predicate calls an atom
+%    prove_all(["Is"| _], T, _, _), !.
+
+prove_all(["what"| _],[Last],_, Ind) :-
+    call(Last),
+    noun([Noun | _],_,_, [Last | _],_),
+    explanation(Noun, Last, Ind, PrintAns),
+    write(PrintAns), flush_output(current_output).
+
+prove_all(["how"| _],[Last],_, _) :-
+    adj([Adj | _],_,_, [Last | _],_),
+    adj_to_noun(Adj, Val, Noun),
+    noun([Noun | _], _, _, [Constraint | _], _),
+    explanation(Noun, Constraint, Val, PrintAns),
+    write(PrintAns), flush_output(current_output).
 
 prove_all(_,[],_, _).
 prove_all(_,[H|T],A, Ind) :-
@@ -123,31 +224,77 @@ prove_all(_,[H|T],A, Ind) :-
     prove_all(_, T, A, Ind),
     A is Ind.
 
+
 % To get the input from a line:
+%q(Ans) :-
+%    write("What city would like to ask about? "), flush_output(current_output),
+%    read_line_to_string(user_input, CitySt),
+%    add_facts_to_database(CitySt),
+%    atom_concat(CitySt, "? ", CityQ),
+%    atom_concat("Great! What would you like to know about ", CityQ, PrintPrompt),
+%    write(PrintPrompt), flush_output(current_output),
+%    read_line_to_string(user_input, St),
+%    split_string(St, " -", " ,?.!-", Ln), % ignore punctuation
+%    ask(Ln, Ans).
 q(Ans) :-
-    add_facts_to_database(_),
-    write("Ask me: "), flush_output(current_output),
-    read_line_to_string(user_input, St),
+    write("What city would you like to ask about? "), flush_output(current_output),
+    read_line_to_string(user_input, CitySt),
+    (add_facts_to_database(CitySt)
+    ->  atom_concat(CitySt, "? ", CityQ),
+        atom_concat("Great! What weather facts would you like to know about ", CityQ, PrintPrompt),
+        write(PrintPrompt), flush_output(current_output),
+        ask_questions(Ans)
+    ;   format("We could not find weather data for ~w. Please try again.", [CitySt]), Ans =
+    []).
+%q(Ans) :-
+%    write("No answers\n"),
+%    q(Ans).
+
+%ask_questions(Ans) :-
+%    read_line_to_string(user_input, St),
+%    split_string(St, " -", " ,?.!-", Ln), % ignore punctuation
+%    \+ ask(Ln, Ans),
+%    write("\nDo you have more questions? (y/n) \n"), flush_output(current_output),
+%    read_line_to_string(user_input, YesOrNo),
+%    (YesOrNo == "y" -> ask_questions(Ans) ; fail).
+
+ask_questions(Ans) :-
+    read_line_to_string(user_input, StIn),
+    downcase_atom(StIn, St),
     split_string(St, " -", " ,?.!-", Ln), % ignore punctuation
-    ask(Ln, Ans).
-q(Ans) :-
-    write("No more answers\n"),
-    q(Ans).
+    (catch(ask(Ln, Ans), _, fail) ->
+        write("No answers")
+        ;
+        true),
+    write("\nDo you have more questions? (y/n) \n"), flush_output(current_output),
+    read_line_to_string(user_input, YesOrNoIn),
+    downcase_atom(YesOrNoIn, YesOrNo),
+    (YesOrNo == "y" -> write("Ask away!\n"), ask_questions(Ans) ; fail).
 
 % ---------------- Predicates about weather ----------------
 % ----- Interpretations about the weather to answer questions -----
 % cold(C) is true if the temperature is T and it is below 10 degrees.
-cold(_) :- temperature(T), T =< 10.0.
+cold(T) :- temperature(T), T =< 10.0.
 
 % warm(C) is true if the temperature is T and it is above 10 degrees
-warm(_) :- temperature(T), T > 10.
+warm(T) :- temperature(T), T > 10.0.
 
-windy(_) :- wind_speed(S), S > 10.
+windy(S) :- wind_speed(S), S > 10.0.
 
-% humid(C) is true if the humidity in C is above 50%
-humid(H) :- humidity(_, H), H > 50.
+rainy(P) :- chance_of_rain(P), P > 50.
 
-% umbrella(C) is true if the chance of rain is above
-umbrella(R) :- chance_rain(_, R), R > 50.
+humid(H) :- humidity(H), H > 60.
+
+cloudy(C) :- cloud(C), C > 60.
+
+sunny(C) :- cloud(C), C < 60.
+
+coat(C) :- cold(C).
+
+umbrella(C) :- rainy(C).
+
+raincoat(C) :- rainy(C).
+
+
 
 
